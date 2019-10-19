122797e5bfe392ad31175dfacf7f07f15c05c6f1
Rishabh Raizada <rishabh5102000@gmail.com>
2019-10-19 19:18:23 +0530

    correct files

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../provider/products.dart';
+
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
+  // final String title;
+  // final double price;
+
+  // ProductDetailScreen(this.title, this.price);
   static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
-    final productId = ModalRoute.of(context).settings.arguments as String;
+    final productId =
+        ModalRoute.of(context).settings.arguments as String; // is the id!
     final loadedProduct = Provider.of<Products>(
       context,
       listen: false,
@@ -16,7 +22,6 @@ class ProductDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(loadedProduct.title),
       ),
-      body: Text('Body'),
     );
   }
 }
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -1,33 +1,36 @@
 import 'package:flutter/material.dart';
-import 'product_item.dart';
-import '../provider/products.dart';
 import 'package:provider/provider.dart';
 
+import '../providers/products.dart';
+import './product_item.dart';
+
 class ProductsGrid extends StatelessWidget {
+  final bool showFavs;
+
+  ProductsGrid(this.showFavs);
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
-    final products = productsData.items;
+    final products = showFavs ? productsData.favoriteItems : productsData.items;
     return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+            // builder: (c) => products[i],
+            value: products[i],
+            child: ProductItem(
+                // products[i].id,
+                // products[i].title,
+                // products[i].imageUrl,
+                ),
+          ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
-        mainAxisSpacing: 10,
-        crossAxisSpacing: 10,
         childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
       ),
-      itemBuilder: (ctx, i) => ChangeNotifierProvider(
-        //we dont need c here
-        // so we can use ChangeNotifierProvider.values and put values: products[i
-        // instead of builder function.
-        builder: (c) => products[i],
-        child: ProductItem(
-//          products[i].id,
-//          products[i].title,
-//          products[i].imageUrl,
-            ),
-      ),
-      padding: EdgeInsets.all(10),
-      itemCount: products.length,
     );
   }
 }
