722e7ad70ef8fd7b678304a5e99ea39ea9130551
Rishabh Raizada <rishabh5102000@gmail.com>
2019-10-20 18:17:56 +0530

    added userProductsScreen

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,41 @@
+import '../widgets/app_drawer.dart';
+import '../widgets/user_product_item.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your products'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemBuilder: (_, i) => Column(
+            children: <Widget>[
+              UserProductItem(
+                productsData.items[i].title,
+                productsData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+          itemCount: productsData.items.length,
+        ),
+      ),
+    );
+  }
+}
