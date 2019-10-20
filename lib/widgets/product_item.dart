dcace50ccf97e8b85a194c671388c959431da6ed
Rishabh Raizada <rishabh5102000@gmail.com>
2019-10-20 17:47:46 +0530

    added snackBar

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -57,6 +57,14 @@ class ProductItem extends StatelessWidget {
             ),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text('Added to cart', textAlign: TextAlign.center,),
+                duration: Duration(seconds: 3),
+                action: SnackBarAction(label: 'UNDO', onPressed: () {
+                  cart.removeSingleItem(product.id);
+                },),
+              ),);
             },
             color: Theme
                 .of(context)
